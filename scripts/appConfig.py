5bca2067770ba02bf32637b66ecd365651b097d2
Albert Luis <albertluis94@gmail.com>
2023-04-02 12:59:24 -0700

    removed acct and auth tokens

--- scripts/appConfig.py
+++ scripts/appConfig.py
@@ -1,7 +1,7 @@
 ##Email
-email_from = "albertluis9445@gmail.com"
-email_to = "albertluis9445@gmail.com"
-pswd = "wajeudzlpddfemfg"
+email_from = ""
+email_to = ""
+pswd = ""
 ##SMS
-account_sid = "ACb221ea80db1b430855f4ffa09136b5a5" 
-auth_token = "e340918a56c17920c5c10049cb3a2854"
+account_sid = "" 
+auth_token = ""
